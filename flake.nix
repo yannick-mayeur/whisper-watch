2c0e10ba00b3f4aeacce72920b0a41aff611420b
Yannick Mayeur <yannick.mayeur@proton.me>
2025-05-21 14:27:21 +0200

    chore: nix flake 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Whisper Watch - A service for automated audio/video transcription";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
